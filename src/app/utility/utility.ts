f0be602939bc61559dc17756961ee28e9b1e244e
Jyothsna Kuchimanchi <jyothsna.kuchimanchi@valuelabs.com>
2017-03-08 19:49:15 +0530

    add a utility class for urls

---
+++ src/app/utility/utility.ts
@@ -0,0 +1,7 @@
+export class AppComponent {
+
+    public static BASE_URL = "http://202.89.107.58/";
+    public static LOGIN_URL = BASE_URL+"GVA.API/api/person/getperson?strGVALoginID=";
+    public static UPDATE_PERSON = BASE_URL+"GVA.API/api/Person/UpdatePerson";
+    public static UPLOAD_SURVEY = BASE_URL+"GVA.API/api/DRA/DRA";
+}
